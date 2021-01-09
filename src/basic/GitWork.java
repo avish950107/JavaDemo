a8a6f9b484f304b97afea9e3a76e83dee6a9553a
avish950107 <avish950107@gmail.com>
2021-01-09 22:02:32 +0530

    first commit

---
+++ src/basic/GitWork.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class GitWork {
+	public static void main(String[] args) {
+
+		System.out.println("Hey this is person x");
+
+
+	}
+}
---
+++ src/basic/Hello.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class Hello {
+
+	public static void main(String s[])
+	{
+		
+		System.out.println("Hello World");
+	}
+}
