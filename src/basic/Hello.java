15069edf2fcdcd8e88511e952d639068ae468545
avish950107 <avish950107@gmail.com>
2021-01-09 23:18:30 +0530

    this is asian time zone

--- src/basic/Hello.java
+++ src/basic/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
 	public static void main(String s[])
 	{
 		
-		System.out.println("Hello World");
+		System.out.println("Hello World one");
+		System.out.println("Hello World second");
+		System.out.println("Hello World three");
 	}
 }
